147c23d543b39dd354f0f97cf4ee614346802074
ambreenbano999 <ambreen.bano@paytm.com>
2015-06-23 10:53:06 +0530

    modified

--- que_bnk.js
+++ que_bnk.js
@@ -39,45 +39,7 @@ if((!validator.isNull(Q_number))&&(!validator.isNull(T_time)))
 console.log('hi');
 
 res.send(html);
-//connection
 
-var mysql = require('mysql');
-var HOST = 'localhost';
-var PORT = 8083;
-var MYSQL_USER = 'root';
-var MYSQL_PASS = 'amber12345';
-var DATABASE = 'abcd';
-var TABLE = 'table1';
-
-
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'amber12345',
-  database : 'abcd',
-  PORT : 8083
- 
-});
-
-connection.connect();
-
-connection.query('USE abcd');
-
-
-var dataQuery="INSERT IGNORE INTO table1 (Username,Email) VALUES ('"+userName+"','"+emailId+"')";
-
-
- connection.query( dataQuery, function(err, rows){
-    if(err) {
-      throw err;
-    }else{
-      console.log( rows );
-    }
-  });
-
-
-
-connection.end();
 }//validation if end
 else
 {
