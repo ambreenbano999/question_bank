118a8b2879f09f1edddf25ff0bc5fe85b44c769e
ambreenbano999 <ambreen.bano@paytm.com>
2015-06-24 10:24:27 +0530

    with comment lines

--- database.js
+++ database.js
@@ -1,17 +1,15 @@
+
 var express = require('express');
 var app = express();
-
+//variables
   var Q_number = 1
   var T_time = 1;
   var que="Hi this is question number ";
   var opt = ["option_a", "option_b", "option_c","option_d"];
   var i=1;
 
-//validation checks
-console.log('hi');
 
-//connection
-
+//connection from mysql
 var mysql = require('mysql');
 var HOST = 'localhost';
 var PORT = 8083;
@@ -30,37 +28,35 @@ var connection = mysql.createConnection
   PORT : 8083
  
 });
-
+//make connection
 connection.connect();
 
-connection.query('USE question');
+connection.query('USE question');//database which we want to use
 
+//query for creating table
 connection.query("create table table2(id int,que varchar(50),opt1 varchar(10),opt2 varchar(10),opt3 varchar(10),opt4 varchar(10),time int)");
 
 for (i = 1; i <= 10000; i++) 
 { 
+  //query to insert data in the database
  connection.query("INSERT INTO table2 (id,que,opt1,opt2,opt3,opt4,time) VALUES ('"+ Q_number +"','"+que+Q_number+"','"+opt[0]+"','"+opt[1]+"','"+opt[2]+"','"+opt[3]+"','"+ T_time +"')", function(err, rows)
  {
-  //console.log("write");
+  
     if(err)
     {
-    throw err;
-    }else
-    {
-
-    //console.log( rows );
+      throw err;
     }
   
- });
+ });//query end
 
 Q_number+=1;
+ 
  if(!(T_time=Math.floor(Math.random()*16)))
  {
-T_time=1;
+  T_time=1;
  }
+console.log("Question number "+ Q_number +" is inserted");
+}//for loop end
 
-}
-
-connection.end();
-console.log("write");
+connection.end();//connection from mysql end
 
